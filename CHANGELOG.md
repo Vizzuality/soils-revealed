9e5e90c444cb9855c6478fe6ea26deb5610d897b
“mluena” <maluenarod@gmail.com>
2020-02-27 17:46:04 +0100

    general set up, settings, changelog, project routes

---
+++ CHANGELOG.md
@@ -0,0 +1,23 @@
+# Changelog
+
+## Unreleased
+
+## 2020-02-27
+
+### Added
+
+- _reset.css file added to remove predefined styles.
+- _document.js
+- project routes
+
+## date
+
+### Added
+
+
+### Changed
+
+- _settings.css boosttrap styles override
+
+### Fixed
+	
---
+++ pages/_document.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Document, { Html, Main, NextScript, Head } from 'next/document';
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <meta name="viewport" content="width=1024, initial-scale=1, shrink-to-fit=no" />
+          <meta name="author" content="Vizzuality" />
+          <link rel="manifest" href="/static/site.webmanifest" />
+          <meta name="msapplication-TileColor" content="#72c990" />
+          <meta name="theme-color" content="#ffffff" />
+          <link
+            href="https://fonts.googleapis.com/css?family=Crimson+Text:400,700|Roboto:400,500,700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body id="root">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
