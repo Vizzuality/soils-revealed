ba5a2ae1bf505175721d50ff33bca5e2fb08a6c4
“mluena” <maluenarod@gmail.com>
2020-03-02 00:37:17 +0100

    footer component

---
+++ components/footer/constants.js
@@ -0,0 +1,22 @@
+const footerLinks = [
+  {
+    name: 'Understand Soils',
+    route: 'discover',
+    path: '/discover',
+    title: 'DISCOVER',
+  },
+  {
+    name: 'Explore Soils',
+    route: 'explore',
+    path: '/explore',
+    title: 'GO TO MAP',
+  },
+  {
+    name: 'Related Projects',
+    route: 'narratives',
+    path: '/narratives',
+    title: 'VIEW PROJECTS',
+  },
+];
+
+export default footerLinks;
