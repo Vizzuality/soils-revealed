52074dea8c7d77c2b69fed35787abe27b41af20f
“mluena” <maluenarod@gmail.com>
2020-03-02 16:14:21 +0100

    new icons added

--- components/icons/index.js
+++ components/icons/index.js
@@ -1,12 +1,75 @@
 import React from 'react';
 
 const Icons = () => (
-  <svg aria-hidden="true" style={{ position: 'absolute', width: 0, height: 0, overflow: 'hidden' }} version="1.1" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink">
+  <svg
+    aria-hidden="true"
+    style={{ position: 'absolute', width: 0, height: 0, overflow: 'hidden' }}
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    xlink="http://www.w3.org/1999/xlink"
+  >
     <defs>
       <symbol id="icon-login" viewBox="0 0 29 32">
         <title>login</title>
-        <path strokeLinejoin="miter" strokeLinecap="square" strokeMiterlimit="4" strokeWidth="2.6667" d="M14.667 16v0c-3.681 0-6.667-2.985-6.667-6.667v-1.333c0-3.681 2.985-6.667 6.667-6.667v0c3.681 0 6.667 2.985 6.667 6.667v1.333c0 3.681-2.985 6.667-6.667 6.667z"></path>
-        <path strokeLinejoin="miter" strokeLinecap="square" strokeMiterlimit="4" strokeWidth="2.6667" d="M28 27.877c0-2.4-1.596-4.511-3.912-5.141-2.525-0.689-5.973-1.403-9.421-1.403s-6.896 0.713-9.421 1.403c-2.316 0.631-3.912 2.741-3.912 5.141v2.789h26.667v-2.789z"></path>
+        <path
+          strokeLinejoin="miter"
+          strokeLinecap="square"
+          strokeMiterlimit="4"
+          strokeWidth="2.6667"
+          d="M14.667 16v0c-3.681 0-6.667-2.985-6.667-6.667v-1.333c0-3.681 2.985-6.667 6.667-6.667v0c3.681 0 6.667 2.985 6.667 6.667v1.333c0 3.681-2.985 6.667-6.667 6.667z"
+        ></path>
+        <path
+          strokeLinejoin="miter"
+          strokeLinecap="square"
+          strokeMiterlimit="4"
+          strokeWidth="2.6667"
+          d="M28 27.877c0-2.4-1.596-4.511-3.912-5.141-2.525-0.689-5.973-1.403-9.421-1.403s-6.896 0.713-9.421 1.403c-2.316 0.631-3.912 2.741-3.912 5.141v2.789h26.667v-2.789z"
+        ></path>
+      </symbol>
+      <symbol id="icon-search" viewBox="0 0 32 32">
+        <title>arrow-right</title>
+        <path
+          fill="none"
+          stroke="#fff"
+          strokeLinejoin="miter"
+          strokeLinecap="butt"
+          strokeMiterlimit="4"
+          strokeWidth="1.5238"
+          d="M31.238 16c0 8.416-6.822 15.238-15.238 15.238s-15.238-6.822-15.238-15.238c0-8.416 6.822-15.238 15.238-15.238s15.238 6.822 15.238 15.238z"
+        ></path>
+        <path
+          fill="none"
+          stroke="#fff"
+          strokeLinejoin="miter"
+          strokeLinecap="butt"
+          strokeMiterlimit="4"
+          strokeWidth="1.5238"
+          d="M8.381 15.619h13.17"
+        ></path>
+        <path
+          fill="none"
+          stroke="#fff"
+          strokeLinejoin="miter"
+          strokeLinecap="square"
+          strokeMiterlimit="4"
+          strokeWidth="1.5238"
+          d="M17.959 11.429l4.19 4.19-4.19 4.19"
+        ></path>
+      </symbol>
+      <symbol id="icon-search" viewBox="0 0 32 32">
+        <title>search</title>
+        <path
+          fill="#000"
+          d="M26.667 16c0 5.891-4.776 10.667-10.667 10.667s-10.667-4.776-10.667-10.667c0-5.891 4.776-10.667 10.667-10.667s10.667 4.776 10.667 10.667z"
+        ></path>
+        <path
+          fill="#c68e54"
+          d="M26.667 16c0 5.891-4.776 10.667-10.667 10.667s-10.667-4.776-10.667-10.667c0-5.891 4.776-10.667 10.667-10.667s10.667 4.776 10.667 10.667z"
+        ></path>
+        <path
+          fill="#fff"
+          d="M17.636 16.997l3.698 3.698-0.639 0.639-3.698-3.698c-0.666 0.528-1.508 0.843-2.424 0.843-2.157 0-3.906-1.749-3.906-3.906s1.749-3.906 3.906-3.906c2.157 0 3.906 1.749 3.906 3.906 0 0.916-0.315 1.758-0.843 2.424v0zM14.573 17.575c1.658 0 3.003-1.344 3.003-3.003s-1.344-3.003-3.003-3.003c-1.658 0-3.003 1.344-3.003 3.003s1.344 3.003 3.003 3.003z"
+        ></path>
       </symbol>
     </defs>
   </svg>
