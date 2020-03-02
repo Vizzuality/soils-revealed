828f0dc17630ca3e6e244c1dc854351f25747c79
“mluena” <maluenarod@gmail.com>
2020-03-02 17:04:54 +0100

    home map component

--- pages/home/component.js
+++ pages/home/component.js
@@ -3,11 +3,13 @@ import React from 'react';
 import StaticPage from 'layout/static-page';
 import Intro from './intro';
 import Challenge from './challenge';
+import Map from './map';
 
 const HomePage = () => (
   <StaticPage className="p-home">
     <Intro />
     <Challenge />
+    <Map />
   </StaticPage>
 );
 
--- pages/home/map/component.js
+++ pages/home/map/component.js
@@ -1,38 +1,39 @@
 import React from 'react';
+
 import { Link } from 'lib/routes';
-import Modal from 'components/modal';
 import SectionTitle from 'components/section-title';
 import Button from 'components/button';
 
 import './style.scss';
 
-const MapComponent = () => (
-  <div className="c-home-map">
-    <Modal isOpen={true}>
-      <div className="map-background">
-        <SectionTitle info={{ index: 2, text: 'Discover' }} />
-        <h2>Areas to Wacth</h2>
-        <p>
-          As well as estimating actual carbon storage, we’ve predicted the potential amount of
-          carbon our soils could hold. Using that, you can see the areas where our action can make
-          the biggest impact.{' '}
-        </p>
-        <div className="buttons">
-          <Button className="-secondary">
-            {/* TO DO: change to the correct route when we get it */}
-            <Link route="/about">
-              <a>Areas to watch</a>
-            </Link>
-          </Button>
-          <Button className="-primary">
-            <Link route="/discover">
-              <a>Discover</a>
-            </Link>
-          </Button>
-        </div>
+const Map = () => (
+  <div className="c-home-map" style={{ backgroundImage: `url(images/homeMapBackground.png)` }}>
+    <div
+      className="map-content"
+      style={{ backgroundImage: `url(images/homeMapBlurryBackground.png)` }}
+    >
+      <SectionTitle info={{ index: 3, text: 'Discover carbon' }} />
+      <h2>Areas to Watch</h2>
+      <p className="section-description">
+        As well as estimating actual carbon storage, we’ve predicted the potential amount of carbon
+        our soils could hold. Using that, you can see the areas where our action can make the
+        biggest impact.
+      </p>
+      <div className="buttons">
+        <Button className="-secondary">
+          {/* TO DO: change to the correct route when we get it */}
+          <Link route="/about">
+            <a>Areas to watch</a>
+          </Link>
+        </Button>
+        <Button className="-primary">
+          <Link route="/discover">
+            <a>Discover</a>
+          </Link>
+        </Button>
       </div>
-    </Modal>
+    </div>
   </div>
 );
 
-export default MapComponent;
+export default Map;
