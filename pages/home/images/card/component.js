6af26f8e2e643adfb3746276f0450fc26770b113
“mluena” <maluenarod@gmail.com>
2020-03-03 13:07:43 +0100

    card prop types added

--- pages/home/images/card/component.js
+++ pages/home/images/card/component.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import PropType from 'prop-types';
 import Icon from 'components/icon';
 
 import './style.scss';
 
 const Card = ({ info }) => (
   <div className="c-card">
-    <img src={`/images${info.imageUrl}`} alt="Soils Revealed logo" />
+    <img src={`/images${info.imageUrl}`} alt={info.title} />
     <div className="card-content">
       <div className="card-head">
         <Icon name="dots-dark" />
@@ -19,5 +20,14 @@ const Card = ({ info }) => (
     </div>
   </div>
 );
+
+Card.propTypes = {
+  info: PropType.shape({
+    title: PropType.string.isRequired,
+    imageUrl: PropType.string.isRequired,
+    category: PropType.string.isRequired,
+    description: PropType.string.isRequired,
+  }),
+};
 
 export default Card;
