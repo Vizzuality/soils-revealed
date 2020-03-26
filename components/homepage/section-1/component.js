4f5bb09f4764d3e0a666860779fa9a079b778d0e
Clément Prod'homme <contact@clementprodhomme.fr>
2020-03-26 17:23:39 +0000

    Align the scroll text elem with the depth vis

--- components/homepage/section-1/component.js
+++ components/homepage/section-1/component.js
@@ -7,24 +7,26 @@ import './style.scss';
 
 const Section1 = () => (
   <section className="c-homepage-section-1">
-    <div className="container text-center">
-      <div className="row mb-5">
-        <div className="col title">Soil carbon can help combat climate change.</div>
-      </div>
-      <div className="row mb-4">
-        <div className="col intro">
-          Soils Revealed can help you find places with the greatest potential to increase soil
-          organic carbon and advance food security, water quality and climate action.
+    <div className="scroll-text-container">
+      <div className="container text-center">
+        <div className="row mb-5">
+          <div className="col title">Soil carbon can help combat climate change.</div>
+        </div>
+        <div className="row mb-4">
+          <div className="col intro">
+            Soils Revealed can help you find places with the greatest potential to increase soil
+            organic carbon and advance food security, water quality and climate action.
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <Link to="explore">
+              <a className="btn btn-primary btn-fixed-width">Explore</a>
+            </Link>
+          </div>
         </div>
       </div>
-      <div className="row">
-        <div className="col">
-          <Link to="explore">
-            <a className="btn btn-primary btn-fixed-width">Explore</a>
-          </Link>
-        </div>
-      </div>
-      <div className="scroll-text">
+      <div className="text-center scroll-text">
         Scroll to discover
         <Icon name="bottom-arrow" />
       </div>
