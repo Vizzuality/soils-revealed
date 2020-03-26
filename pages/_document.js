2ab38d6712f6daa272c4618362c0dcdc147cdcd5
Clément Prod'homme <contact@clementprodhomme.fr>
2020-03-26 18:04:56 +0000

    Refine the spacing and font weight

--- pages/_document.js
+++ pages/_document.js
@@ -16,7 +16,7 @@ export default class MyDocument extends Document {
           <meta name="viewport" content="width=1024, initial-scale=1, shrink-to-fit=no" />
           <meta name="author" content="Vizzuality" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Crimson+Text&family=Roboto+Mono:wght@400;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Crimson+Text&family=Roboto+Mono:wght@400;500;700&display=swap"
             rel="stylesheet"
           />
         </Head>
