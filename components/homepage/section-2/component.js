a622ccdae897868b3768b47cd5a1f5fb739f634e
Clément Prod'homme <contact@clementprodhomme.fr>
2020-03-27 11:36:59 +0000

    Add the visualisation of section 2

--- components/homepage/section-2/component.js
+++ components/homepage/section-2/component.js
@@ -16,6 +16,53 @@ const Section2 = ({ configuration }) => (
           three times as much as the atmosphere.
         </div>
       </div>
+      <div className="row">
+        <div className="col-sm-12 col-md-8 offset-md-2 text-right mt-2">
+          <svg width="547" height="203" className="visualization">
+            <defs>
+              <pattern id="b" width="20" height="20" x="-20" y="-20" patternUnits="userSpaceOnUse">
+                <use xlinkHref="#a" transform="scale(.5)" />
+              </pattern>
+              <image
+                id="a"
+                width="40"
+                height="40"
+                xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACgAAAAoCAYAAACM/rhtAAAABGdBTUEAALGN5fIAKQAAAOJJREFUWAntl+EJwyAUhLUjlM7TDtE5O0Q6T2aozQsGAvJQ65dS6PknEJ/HeZ+ihtDR0vNyS9N5tm/HMLcU1UPFFsuoHiomc+6KqnegJFAxYa3TcytQEqiYsLrQ6h0oCVSMxmpZ/OzZmie7nolmdLQdRSKOGrPxZi6k1yPE0z1e52lUc683qsXeSvJksWV3FNY1wdHoZO7TBJWckmtIAF0mqNhiHtVDxWSuYW15JSgJVExYPWYN/1ESqNhfYTVS2OWQTi7r6U1ilHqb7YntjdM7tqjXBisiafyh5BqDKsq+ndwb1zckkQuos6sAAAAASUVORK5CYII="
+              />
+            </defs>
+            <g fill="none" fillRule="evenodd">
+              <g transform="translate(345 1)">
+                <circle
+                  stroke="#FFC112"
+                  strokeWidth="2"
+                  fill="url(#b)"
+                  cx="100.5"
+                  cy="100.5"
+                  r="100.5"
+                />
+                <circle fill="#FFC112" cx="100.5" cy="155.5" r="45.5" />
+              </g>
+              <text transform="translate(-7 1)">
+                <tspan x="39" y="123">
+                  Carbon in the Atmosphere:{' '}
+                </tspan>{' '}
+                <tspan x="107.578" y="143">
+                  860 billion tons
+                </tspan>
+              </text>
+              <text transform="translate(-7 1)">
+                <tspan x="40.367" y="47">
+                  Organic Carbon in Soils:{' '}
+                </tspan>{' '}
+                <tspan x="6.762" y="67">
+                  2400 billion tons / 0-200 cm
+                </tspan>
+              </text>
+              <path strokeWidth="2" d="M348.346 53H244M399.346 129H245" />
+            </g>
+          </svg>
+        </div>
+      </div>
     </div>
   </section>
 );
