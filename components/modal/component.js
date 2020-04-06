7f71c3f7277e793d04f86d5423ce080d6ddf1c93
Clément Prod'homme <contact@clementprodhomme.fr>
2020-04-06 14:37:37 +0100

    Add a modal component

--- components/modal/component.js
+++ components/modal/component.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import Modal from 'react-modal';
+import PropTypes from 'prop-types';
+import ReactModal from 'react-modal';
+
+import Icon from 'components/icon';
+
 import './style.scss';
 
-const customStyles = {
-  overlay: {
-    background: 'rgba(0, 0, 0, 0.7)'
-  },
+ReactModal.setAppElement('#root');
+
+const Modal = ({ open, onClose, title, children, className }) => (
+  <ReactModal
+    isOpen={open}
+    onRequestClose={onClose}
+    contentLabel={title}
+    className={['c-modal', ...(className ? [className] : [])].join(' ')}
+  >
+    <button type="button" className="btn btn-outline-primary close-button" onClick={onClose}>
+      <Icon name="close" />
+    </button>
+    <div className="content">{children}</div>
+  </ReactModal>
+);
+
+Modal.propTypes = {
+  open: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
 };
 
-Modal.setAppElement('#root');
-
-export default (props) => {
-  const { children, ...domProps } = props;
-
-  return (
-    <Modal
-      style={customStyles}
-      {...domProps}
-    >
-      {children}
-    </Modal>
-  );
+Modal.defaultProps = {
+  className: null,
 };
+
+export default Modal;
