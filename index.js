13142fa462efb60d200742d2ec71829fa961c06f
Clément Prod'homme <contact@clementprodhomme.fr>
2020-04-29 09:55:48 +0100

    Add a basic API for the GEE layers

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const next = require('next');
 
+// const gee = require('./api/gee');
+
 const port = parseInt(process.env.PORT, 10) || 3000;
 const isDev = process.env.NODE_ENV !== 'production';
 
@@ -10,6 +12,8 @@ const handle = app.getRequestHandler();
 app.prepare().then(() => {
   const server = express();
 
+  // server.get('/api/gee/:imageId/:z/:x/:y', gee);
+  server.get('/api/*', (req, res) => res.status(404).end());
   server.all('*', (req, res) => handle(req, res));
 
   server.listen(port, err => {
