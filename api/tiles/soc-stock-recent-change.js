ee9496a7ff57ac776008de734ed1a47d11196b4b
Clément Prod'homme <contact@clementprodhomme.fr>
2020-07-24 10:43:36 +0100

    Add the recent tiles service

--- api/tiles/soc-stock-recent-change.js
+++ api/tiles/soc-stock-recent-change.js
@@ -1,3 +1,45 @@
-module.exports = (_, res) => {
-  res.status(404).end();
+const ee = require('@google/earthengine');
+const axios = require('axios').default;
+
+const RAMP = `
+  <RasterSymbolizer>
+    <ColorMap extended="false" type="ramp">
+    <ColorMapEntry color="#B30200" quantity="-1.6" opacity="1" />
+    <ColorMapEntry color="#E34A33" quantity="-0.8" />
+    <ColorMapEntry color="#FC8D59" quantity="-0.4" />
+    <ColorMapEntry color="#FDCC8A" quantity="-0.2" />
+    <ColorMapEntry color="#FFFFCC" quantity="0" />
+    <ColorMapEntry color="#A1DAB4" quantity="0.2" />
+    <ColorMapEntry color="#31B3BD" quantity="0.4" />
+    <ColorMapEntry color="#1C9099" quantity="0.8" />
+    <ColorMapEntry color="#066C59" quantity="1.6" />
+    </ColorMap>
+  </RasterSymbolizer>
+`;
+
+module.exports = ({ params: { year1, year2, x, y, z } }, res) => {
+  try {
+    const collection = ee.ImageCollection('projects/soils-revealed/Recent/SOC_stocks');
+
+    const image = collection
+      .filterDate(`${year2}-01-01`, `${year2}-12-31`)
+      .first()
+      .subtract(collection.filterDate(`${year1}-01-01`, `${year1}-12-31`).first())
+      .sldStyle(RAMP);
+
+    image.getMap({}, async ({ formatTileUrl }) => {
+      const url = formatTileUrl(x, y, z);
+      const serverPromise = axios.get(url, {
+        headers: { Accept: 'image/*' },
+        responseType: 'arraybuffer',
+      });
+      await serverPromise.then(serverResponse => {
+        res.set('Content-Type', 'image/png');
+        res.set('Cache-Control', 'public,max-age=604800');
+        return res.send(Buffer.from(serverResponse.data));
+      });
+    });
+  } catch (e) {
+    res.status(404).end();
+  }
 };
--- api/tiles/soc-stock-recent-timeseries.js
+++ api/tiles/soc-stock-recent-timeseries.js
@@ -1,3 +1,42 @@
-module.exports = (_, res) => {
-  res.status(404).end();
+const ee = require('@google/earthengine');
+const axios = require('axios').default;
+
+const RAMP = `
+  <RasterSymbolizer>
+    <ColorMap extended="false" type="ramp">
+      <ColorMapEntry color="#E18D67" quantity="5" opacity="1" />
+      <ColorMapEntry color="#CB5A3A" quantity="20" />
+      <ColorMapEntry color="#9D4028" quantity="50" />
+      <ColorMapEntry color="#6D2410" quantity="75" />
+      <ColorMapEntry color="#380E03" quantity="200" />
+    </ColorMap>
+  </RasterSymbolizer>
+`;
+
+module.exports = ({ params: { year, x, y, z } }, res) => {
+  try {
+    const image = ee
+      .Image(
+        ee
+          .ImageCollection('projects/soils-revealed/Recent/SOC_stocks')
+          .filterDate(`${year}-01-01`, `${year}-12-31`)
+          .first()
+      )
+      .sldStyle(RAMP);
+
+    image.getMap({}, async ({ formatTileUrl }) => {
+      const url = formatTileUrl(x, y, z);
+      const serverPromise = axios.get(url, {
+        headers: { Accept: 'image/*' },
+        responseType: 'arraybuffer',
+      });
+      await serverPromise.then(serverResponse => {
+        res.set('Content-Type', 'image/png');
+        res.set('Cache-Control', 'public,max-age=604800');
+        return res.send(Buffer.from(serverResponse.data));
+      });
+    });
+  } catch (e) {
+    res.status(404).end();
+  }
 };
