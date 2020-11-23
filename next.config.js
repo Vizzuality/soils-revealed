f7952da638013698ec440676ff5e6bebf0d3804b
Clément Prod'homme <contact@clementprodhomme.fr>
2020-11-23 16:35:28 +0000

    Add a script to analyse the bundles

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 const withSass = require('@zeit/next-sass');
 const dotenv = require('dotenv').config();
 
-module.exports = withSass({
-  env: {
-    ...dotenv.parsed,
-  },
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
 });
+
+module.exports = withBundleAnalyzer(
+  withSass({
+    env: {
+      ...dotenv.parsed,
+    },
+  })
+);
