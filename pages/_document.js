bf0e2b38ef8370a41d6f9a7eaf666456469860a0
Clément Prod'homme <contact@clementprodhomme.fr>
2020-11-23 18:24:23 +0000

    Fix Next.js warning

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,6 @@ export default class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-          <meta name="viewport" content="width=1024, initial-scale=1, shrink-to-fit=no" />
           <meta name="author" content="Vizzuality" />
           <link
             href="https://fonts.googleapis.com/css2?family=Crimson+Text&family=Roboto+Mono:wght@400;500;700&display=swap"
